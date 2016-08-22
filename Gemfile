07f68d43b79da07eb10b2a7a62302ac805fc6702
tanete <tanete772@outlook.com>
2016-08-22 06:24:05 +0000

    Conclusion: greate gem

--- Gemfile
+++ Gemfile
@@ -27,6 +27,12 @@ gem 'jbuilder', '~> 2.5'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
+gem 'faker'
+gem 'chartkick'
+gem 'hightop'
+gem 'groupdate'
+gem 'pg'
+
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'statistics#index'
+  resources :charts, only: [] do
+    collection do
+      get 'sporters_by_age'
+      get 'sporters_top_by_age'
+      get 'sporters_by_country'
+      get 'results_by_country'
+      get 'competitions_by_year'
+    end
+  end
 end
